ba0842cb908fd03a7e9e2f69a8488837243a85a5
McQbis <kubismaciej379@gmail.com>
2025-05-20 11:51:53 +0200

    photo deletion

--- server/photos/urls.py
+++ server/photos/urls.py
@@ -1,8 +1,9 @@
 from django.urls import path
-from .views import UploadEncryptedPhotoView, ViewDecryptedPhoto, TemporaryDecryptedPhotoView
+from .views import UploadEncryptedPhotoView, ViewDecryptedPhoto, TemporaryDecryptedPhotoView, DeletePhotoView
 
 urlpatterns = [
     path('upload-photo/', UploadEncryptedPhotoView.as_view(), name='upload-photo'),
     path('view/<int:photo_id>/', ViewDecryptedPhoto.as_view(), name='view-photo'),
     path('temp-view/<str:signed_value>/', TemporaryDecryptedPhotoView.as_view()),
+    path('delete-photo/<int:photo_id>/', DeletePhotoView.as_view(), name='delete-photo'),
 ]
