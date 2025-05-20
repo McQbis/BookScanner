dd0a934dfda3ffe984abdb2fed3a9d50b0354075
McQbis <kubismaciej379@gmail.com>
2025-05-20 12:22:09 +0200

    delete photos and key when user delete account

--- server/photos/models.py
+++ server/photos/models.py
@@ -2,7 +2,7 @@ from django.db import models
 from django.contrib.auth.models import User
 
 class EncryptedPhoto(models.Model):
-    user = models.ForeignKey(User, on_delete=models.CASCADE)
+    user = models.ForeignKey(User, on_delete=models.CASCADE, related_name='photos')
     file = models.FileField(upload_to='photos/', default='')
     original_filename = models.CharField(max_length=255, default='')
     uploaded_at = models.DateTimeField(auto_now_add=True)
