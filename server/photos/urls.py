73fc419960eefed82147a191ddeb03b3f06e81f8
McQbis <kubismaciej379@gmail.com>
2025-05-20 11:19:54 +0200

    exncrypted photos storage

--- server/photos/urls.py
+++ server/photos/urls.py
@@ -1,6 +1,8 @@
 from django.urls import path
-from .views import UploadPhotoView
+from .views import UploadEncryptedPhotoView, ViewDecryptedPhoto, TemporaryDecryptedPhotoView
 
 urlpatterns = [
-    path('upload-photo/', UploadPhotoView.as_view(), name='upload-photo'),
+    path('upload-photo/', UploadEncryptedPhotoView.as_view(), name='upload-photo'),
+    path('view/<int:photo_id>/', ViewDecryptedPhoto.as_view(), name='view-photo'),
+    path('temp-view/<str:signed_value>/', TemporaryDecryptedPhotoView.as_view()),
 ]
